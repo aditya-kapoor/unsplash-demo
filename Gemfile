f2873191a8edd89e1133b1d346d30da7ab3c613f
Aditya Kapoor <adityakapoor.mait@gmail.com>
2022-10-12 04:25:36 +0530

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "2.6.8"
+
+gem "pg"
+gem "rails", "~> 6.0.6"
+gem "unsplash"
+gem "pry"
+
+group :development do
+  gem "listen"
+  gem "awesome_print"
+end
---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "2G1l-Q9id69EI3qw-ylsboDRYiZGjiC-XFvCwrath54"
+  config.application_secret = "McMRZ5Yrgo0jkom0g4UFXQ5rP95I1uM7hDphB_9jUIE"
+  config.application_redirect_uri = "localhost:3000/oauth/callback"
+  config.utm_source = "fabrikator_testing_app"
+end
